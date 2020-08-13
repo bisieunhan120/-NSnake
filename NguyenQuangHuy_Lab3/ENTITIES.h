fffd3c01a6b1668b8b61852211a308c9e2c728aa
Administrator <Administrator@PC476>
2020-08-13 10:25:35 +0700

    hhjj

---
+++ NguyenQuangHuy_Lab3/ENTITIES.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+#include "GLOBAL.h"
+
+#define SNAKE_MAXLEN 100 /* chiều dài tối đa của rắn */
+#define SNAKE_INITLEN 2 /* chiều dài bắt đầu của rắn */
+
+typedef struct snake {
+	COORD coords[SNAKE_MAXLEN]; /* rắn là™ 2D */
+	int len; /* độ dài hiện thời của rắn */
+} SNAKE;
+
+typedef struct fruit {
+	COORD loc; /* vị trí mồi */
+	Uint32 color; /* màu sắc*/
+} FRUIT;
+
+/* vẽ 1 ô của thân rắn hoặc đầu rắn */
+void snakeCell(COORD c, SDL_bool head);
+
+/* khời tạo rắn */
+void snakeInit(SNAKE* s);
+
+/* vẽ rắn */
+void snakeDraw(const SNAKE* s);
+
+/* cập nhật trạng thái của rắn */
+void snakeUpdate(SNAKE* s);
+
+/* phát sinh mồi*/
+void fruitGen(FRUIT* f, const SNAKE* s);
+
+/* vẽ mồi */
+void fruitDraw(const FRUIT* f);
---
+++ NguyenQuangHuy_Lab3/GAME.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+//#include "stdafx.h"
+#include "GLOBAL.h"
+#include "GAME.h"
+#include "ENTITIES.h"
+
+#define GAME_MAX_FPS 12 /* giới hạn tới tốc độ khung 12fps */
+#define GAME_FRAME_TIME (1000 / GAME_MAX_FPS)
+
+typedef struct game /* cấu trúc trò chơi bao gồm */
+{
+	FRUIT f; /* mồi */
+	SNAKE s; /* rắn */
+	DIRECTION dir; /* hướng di chuyển rắn*/
+	SDL_bool running; /* cờ trạng thái hoạt động */
+} GAME;
+
+/* khời tạo*/
+void gameInit(GAME* g, SDL_bool fullscreen);
+
+/* kết thúc trò chơi */
+void gameShutdown(GAME* g);
+
+/* vòng lặp trò chơi ádasdas*/
+void gameLoop(GAME* g);
+
+/* cập nhật trạng thái trò chơi */
+void gameUpdate(GAME* g);
+
+/* dựng hình cho cho 1 frame */
+void gameDraw(GAME* g);
+
+/* xử lý input */
+void gameInput(GAME* g);
